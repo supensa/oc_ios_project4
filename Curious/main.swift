aef1341d7bafe4ac70844cb78181fa1c7f803ff4
Spencer Forrest <spencer.forrest.devmob@gmail.com>
2017-12-24 18:57:14 +0100

    Initial Commit

---
+++ Curious/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Curious
+//
+//  Created by Spencer Forrest on 24/12/2017.
+//  Copyright © 2017 Spencer Forrest. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
